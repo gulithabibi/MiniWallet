1870349cf209b69adebea956a32c751d3470e548
Gulit Habibi <gulit.habibi@xtremax.com>
2022-06-18 20:39:17 +0700

    update

---
+++ MiniWalletApi/Repositories/Interfaces/IWithdrawalRespository.cs
@@ -0,0 +1,17 @@
+﻿using MiniWalletApi.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MiniWalletApi.Repositories.Interfaces
+{
+    public interface IWithdrawalRespository
+    {
+        Task<IEnumerable<Withdrawal>> Find();
+        Task<Withdrawal> FindByID(Guid id);
+        Task<Withdrawal> Create(Withdrawal deposit);
+        Task<Withdrawal> Update(Withdrawal deposit);
+        Task Delete(Guid id);
+    }
+}
---
+++ MiniWalletApi/Repositories/WithdrawalRepository.cs
@@ -0,0 +1,57 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MiniWalletApi.Libraries;
+using MiniWalletApi.Models;
+using MiniWalletApi.Repositories.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MiniWalletApi.Repositories
+{
+    public class WithdrawalRepository : IWithdrawalRespository
+    {
+        private readonly MiniWalletContext _context;
+
+
+        public WithdrawalRepository(MiniWalletContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<IEnumerable<Withdrawal>> Find()
+        {
+            return _context.Withdrawals.ToList();
+        }
+
+        public async Task<Withdrawal> FindByID(Guid id)
+        {
+            return await _context.Withdrawals.FindAsync(id);
+        }
+
+        public async Task<Withdrawal> Create(Withdrawal withdrawal)
+        {
+            _context.Withdrawals.Add(withdrawal);
+            await _context.SaveChangesAsync();
+
+            return withdrawal;
+        }
+
+        public async Task<Withdrawal> Update(Withdrawal withdrawal)
+        {
+            _context.Withdrawals.Update(withdrawal);
+            _context.Entry(withdrawal).State = EntityState.Modified;
+            await _context.SaveChangesAsync();
+
+            return withdrawal;
+        }
+
+        public async Task Delete(Guid id)
+        {
+
+            var withdrawal = await _context.Withdrawals.FindAsync(id);
+            _context.Withdrawals.Remove(withdrawal);
+            await _context.SaveChangesAsync();
+        }
+    }
+}
